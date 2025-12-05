769c93b7dca8730b3246b666b2ee5c8363317e88
Meerthika <meerthikasrrajendrans@gmail.com>
2025-12-05 11:42:01 +0530

    Add file and folders for exception

--- user-service/src/main/java/com/vircsam/model/User.java
+++ user-service/src/main/java/com/vircsam/model/User.java
@@ -11,9 +11,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	
 	
