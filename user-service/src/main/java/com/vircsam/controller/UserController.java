bbed41a46e40df2924cadec8435c66bfbb0c0ef5
Meerthika <meerthikasrrajendrans@gmail.com>
2025-12-05 08:12:14 +0530

    Update User endpoint created

--- user-service/src/main/java/com/vircsam/controller/UserController.java
+++ user-service/src/main/java/com/vircsam/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.vircsam.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +28,18 @@ public class UserController {
 	}
 	
 	@GetMapping("/api/users")
-	public User getUser() {
-		User user = new User();
-		user.setEmail("meerthika@gmail.com");
-		user.setFullName("Meerthika");
-		user.setPhone("934234523");
-		user.setRole("Owner");
+	public List<User> getUser() {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/api/users/{id}")
+	public User getUserById(@PathVariable Long id) throws Exception {
+		Optional<User> otp = userRepository.findById(id);
+		if(otp.isPresent()) {
+			return otp.get();
+		}
 		
-		return user;
+		throw new Exception("User not found with id: "+ id);
+		
 	}
 }
